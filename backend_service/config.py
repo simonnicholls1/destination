382b9a05e2c5589ac8eac8414d6246ff6c6733c4
s-nicholls <simon.nicholls@arabesque.com>
2023-03-06 11:39:59 +0000

    get action to work

--- backend_service/config.py
+++ backend_service/config.py
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
     secret_key: str
     password_salt: str
     algorithm: str
-    access_token_expire_minutes: str
+    access_token_expire_minutes: int
     mail_api_key: str
     email_template_id: str
     from_email: str
@@ -21,7 +21,7 @@ env_path = os.environ.get('ENV_FILE_PATH', '../prod.env')
 settings = Settings(_env_file=env_path, _env_file_encoding='utf-8')
 
 # convert access_token_expire_minutes to int
-settings.access_token_expire_minutes = int(settings.access_token_expire_minutes)
+#settings.access_token_expire_minutes = int(settings.access_token_expire_minutes)
 
 if not settings.cloud_sql_connection_name and not settings.database_hostname:
     raise ValueError("One of cloud_sql_connection_name or database_hostname must be provided")
