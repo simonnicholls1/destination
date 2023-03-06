24ecafeaa3109ac560192f7b96202957b7695d57
s-nicholls <simon.nicholls@arabesque.com>
2023-03-06 09:18:05 +0000

    get action to work

--- backend_service/config.py
+++ backend_service/config.py
@@ -2,7 +2,6 @@ from pydantic import BaseSettings
 import os
 
 class Settings(BaseSettings):
-    database_hostname: str
     database_port: str
     database_password: str
     database_name: str
@@ -15,7 +14,15 @@ class Settings(BaseSettings):
     email_template_id: str
     from_email: str
     cloud_sql_connection_name: str = None
+    database_hostname: str = None
 
 
 env_path = os.environ.get('ENV_FILE_PATH', '../prod.env')
 settings = Settings(_env_file=env_path, _env_file_encoding='utf-8')
+
+# convert access_token_expire_minutes to int
+settings.access_token_expire_minutes = int(settings.access_token_expire_minutes)
+
+if not settings.cloud_sql_connection_name and not settings.database_hostname:
+    raise ValueError("One of cloud_sql_connection_name or database_hostname must be provided")
+
