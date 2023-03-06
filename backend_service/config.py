16c290aa320cae1ded5a53e5e68a7fa24f63d996
s-nicholls <simon.nicholls@arabesque.com>
2023-03-06 13:47:43 +0000

    get action to work

--- backend_service/config.py
+++ backend_service/config.py
@@ -1,6 +1,7 @@
 from pydantic import BaseSettings
 import os
 
+
 class Settings(BaseSettings):
     database_port: str
     database_password: str
@@ -17,9 +18,10 @@ class Settings(BaseSettings):
     database_hostname: str = None
 
 
+print('test' + os.environ.get('cloud_sql_connection_name'))
+
 env_path = os.environ.get('ENV_FILE_PATH', '../prod.env')
 settings = Settings(_env_file=env_path, _env_file_encoding='utf-8')
 
 if not settings.cloud_sql_connection_name and not settings.database_hostname:
     raise ValueError("One of cloud_sql_connection_name or database_hostname must be provided")
-
