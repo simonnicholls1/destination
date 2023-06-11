16e57a6e1451144d8d1f10a2d8f3770a292ffab9
s-nicholls <simon.nicholls@arabesque.com>
2023-06-11 14:43:23 +0200

    added blog page and featured blog cards

--- frontend_app/src/pages/Blog.js
+++ frontend_app/src/pages/Blog.js
@@ -2,17 +2,19 @@ import React, { useState, useEffect } from 'react';
 import Navbar from '../components/common/NavBar';
 import Footer from '../components/common/Footer';
 import BlogCard from '../components/blog/BlogCards'
+import FeaturedBlogCard from '../components/blog/FeaturedBlogCard'
 
 const Blog = () => {
     const [posts, setPosts] = useState([]);
+    const [featuredPosts, setFeaturedPosts] = useState([]);
     const [error, setError] = useState(null);
 
     useEffect(() => {
-      const fetchData = async () => {
+    const fetchAllPosts = async () => {
         try {
           const response = await fetch('destinationapi/posts/');
           if(!response.ok) {
-              throw new Error(response.statusText);
+              //throw new Error(response.statusText);
           }
           const data = await response.json();
           setPosts(data);
@@ -20,7 +22,22 @@ const Blog = () => {
           setError(error);
         }
       };
-      //fetchData();
+
+      const fetchFeaturedPosts = async () => {
+        try {
+          const response = await fetch('destinationapi/posts?featured=4');
+          if(!response.ok) {
+              //throw new Error(response.statusText);
+          }
+          const data = await response.json();
+          setFeaturedPosts(data);
+        } catch(error) {
+          setError(error);
+        }
+      };
+
+      fetchAllPosts();
+      fetchFeaturedPosts();
     }, []);
 
     if (error) {
@@ -32,17 +49,22 @@ const Blog = () => {
   }
 
   return (
-    <>
-      <Navbar />
-      <div>
-        <h1>My Blog</h1>
-        {posts.map((post, index) => (
-          <BlogCard key={index} post={post} /> // Use the BlogCard component
-        ))}
-      </div>
-      <Footer />
-    </>
-  );
+      <>
+        <Navbar />
+        <div>
+          <h1>My Blog</h1>
+          <h2>Featured Blogs</h2>
+          {featuredPosts.map((post, index) => (
+            <FeaturedBlogCard key={index} blog={post} />
+          ))}
+          <h2>Latest Blogs</h2>
+          {posts.map((post, index) => (
+            <BlogCard key={index} blog={post} />
+          ))}
+        </div>
+        <Footer />
+      < />
+    );
 };
 
 export default Blog;
